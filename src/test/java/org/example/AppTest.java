3dcc0e22b84d6d29a6113a13afbe1d7937eb285e
Grisha812 <90569472+Grisha812@users.noreply.github.com>
2022-02-23 13:09:35 +0300

    Add files via upload

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,30 +1,62 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.RepeatedTest;
-import org.junit.platform.commons.logging.LoggerFactory;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    //private static logger logger = (logger) LoggerFactory.getLogger(AppTest.class);
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+
+abstract public class AppTest {
+    protected WebDriver driver;
+
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        BaseSeleniumPage.setDriver(driver);
     }
 
-    @Test
-    @RepeatedTest(value = 3)
-    void testMy(){
-        System.out.println("My ");
-    }
 
+    @After
+    public void tearDown(){
+        driver.close();
+        driver.quit();
+    }
 }
+
+/*
+    //private static logger logger = (logger) LoggerFactory.getLogger(AppTest.class);
+    static WebDriver webDriver;
+
+    @BeforeAll
+    static void setDriver(){
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        //options.addArguments("--headless"); //данная опция позволяет запускать тесты не открывая сам браузер (для экономии ресурса, т.к. браузер много кушает)
+
+        //options.setPageLoadTimeout(Duration.ofSeconds(10));
+        webDriver = new ChromeDriver(options);
+
+
+    }
+
+*/
